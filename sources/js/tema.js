2f7ff64ccd4ae1e01b663b814848f9588a557b8b
iivaaan_s <124355992+vaN1N@users.noreply.github.com>
2025-10-14 17:40:49 +0000

    Update tema.js

--- sources/js/tema.js
+++ sources/js/tema.js
@@ -2,10 +2,10 @@ document.addEventListener("DOMContentLoaded", function () {
     const toggleBtn = document.getElementById("toggleBtn");
     if (toggleBtn) {
       toggleBtn.addEventListener("click", function () {
-        // tu lógica aquí
       });
     } else {
       console.warn("No se encontró el botón con id 'toggleBtn'");
     }
   });
+
   
