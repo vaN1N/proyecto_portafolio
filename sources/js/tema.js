416eb497fd01da8114ebd1509e141ef9a7021382
iivaaan_s <124355992+vaN1N@users.noreply.github.com>
2025-12-29 15:52:37 +0000

    Cambio de tema Oscuro a claro

--- sources/js/tema.js
+++ sources/js/tema.js
@@ -1,11 +1,15 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const toggleBtn = document.getElementById("toggleBtn");
-    if (toggleBtn) {
-      toggleBtn.addEventListener("click", function () {
-      });
-    } else {
-      console.warn("No se encontró el botón con id 'toggleBtn'");
-    }
-  });
+const themeToggle = document.getElementById('theme-toggle');
+const themeIcon = themeToggle.querySelector('span');
 
-  
+themeToggle.addEventListener('click', () => {
+    const html = document.documentElement;
+    
+    // si esta en oscuro, pasamos a claro
+    if (html.getAttribute('data-bs-theme') === 'dark') {
+        html.setAttribute('data-bs-theme', 'light');
+        themeIcon.textContent = 'dark_mode'; // icono de luna para el modo oscuro
+    } else { // si esta en claro, pasamos a oscuro
+        html.setAttribute('data-bs-theme', 'dark');
+        themeIcon.textContent = 'light_mode'; // icono de sol para aclarar
+    }
+});
